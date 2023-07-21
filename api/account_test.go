99630d1cd4532fd869c57d938d1038794c1044ba
hank <akysn564@gmail.com>
2023-07-21 17:32:30 +0800

    test: dbMock part01

---
+++ api/account_test.go
@@ -0,0 +1,32 @@
+package api
+
+import (
+	mockdb "simpleBank/db/mock"
+	db "simpleBank/db/sqlc"
+	"simpleBank/util"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+)
+
+func TestGetAccountAPI(t *testing.T) {
+	account := randomAccount()
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	store := mockdb.NewMockStore(ctrl)
+	store.EXPECT().
+		GetAccount(gomock.Any(), gomock.Eq(account.ID)).
+		Times(1).
+		Return(account, nil)
+}
+
+func randomAccount() db.Account {
+	return db.Account{
+		ID:       util.RandomInt(1, 1000),
+		Owner:    util.RandomOwner(),
+		Balance:  util.RandomMoney(),
+		Currency: util.RandomCurrency(),
+	}
+}
